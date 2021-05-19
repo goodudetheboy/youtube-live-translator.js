ea40a69f1d88abde893bf6eab6b01cbd1a57d3e6
Vuong Ho <hochivuong2002@gmail.com>
2021-05-19 19:25:54 +0700

    Temporary recording PC output using Virtual Cable

---
+++ audio_capturing/capture.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const portAudio = require('naudiodon');
+
+// Use this to find out the device id for soundflower
+console.log(portAudio.getDevices());
+
+// create audio io instance
+const ai = new portAudio.AudioIO({
+  inOptions: {
+    channelCount: 2, 
+    sampleFormat: portAudio.SampleFormat16Bit,
+    sampleRate: 44100,
+    deviceId: 17, // put [AUDIO_ID] here
+    closeOnError: true,
+  }
+});
+const ws = fs.createWriteStream('rawAudio.raw');
+ai.pipe(ws);
+ai.start();
+// Wait a while as you record the audio in -- check your sound levels in SoundflowerBed, etc.
+// When you are done...
+setTimeout(function(){ ai.quit() }, 10000);
