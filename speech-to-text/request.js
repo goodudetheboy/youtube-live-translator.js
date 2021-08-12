ee293403ed47dd86df13bf70f2086bb66eb07f08
Vuong Ho <hochivuong2002@gmail.com>
2021-08-12 10:31:18 +0700

    Send request to wit.ai by js instead of python

--- speech-to-text/request.js
+++ speech-to-text/request.js
@@ -1,6 +1,24 @@
-const spawn = require("child_process").spawn;
-const pythonProcess = spawn('python', ["speech-to-text/request.py"]);
+const https = require('https')
+const request = require('request');
+const fs = require('fs');
+require('dotenv').config();
 
-pythonProcess.stdout.on('data', (data) => {
-    console.log(data.toString());
-});
+const WIT_AI_TOKEN = process.env.WIT_AI_TOKEN;
+
+request({
+    url: 'https://api.wit.ai/speech?v=20200513',
+    method: 'POST',
+    headers: {
+      'Content-type' : 'audio/wav',
+      'Authorization' : `Bearer ${ WIT_AI_TOKEN }`
+    },
+    encoding: null,
+    body: fs.createReadStream('./speech-to-text/sample.wav')
+    }, (error, response, body) => {
+        if (error) {
+            // response.json({name : error});
+            console.log(response);
+        } else {
+            console.log(JSON.parse(response.body.toString()));
+        }
+    });
